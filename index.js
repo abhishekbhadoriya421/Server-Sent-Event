7f158c8c4befac6595ff7181fff600b231830cf4
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-09-01 21:53:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+
+
+app.listen(8080, (err) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log('Server is running....');
+    }
+});
