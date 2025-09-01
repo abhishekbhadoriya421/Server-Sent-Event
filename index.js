9f1dbe8e945373f9d750ea75804d1ce1131aefe9
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-09-01 22:12:41 +0530

    handle server side event set connection alive and sent response

--- index.js
+++ index.js
@@ -8,6 +8,27 @@ app.get('/', (req, res) => {
     return res.sendFile(filePath);
 });
 
+app.get('/event', (req, res) => {
+    console.log("Connecting To Event Server");
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+
+    const intervalId = setInterval(() => {
+        res.write(`data: ${new Date().toISOString()}\n\n`);
+    }, 3000);
+
+    res.on('close', () => {
+        console.log("Disconnected");
+        clearInterval(intervalId);
+    });
+});
+
+
+app.get('/home', (req, res) => {
+    return res.json({ "home": "sweet home" });
+})
+
 app.listen(8080, (err) => {
     if (err) {
         console.log(err)
