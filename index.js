f03331fb3225c224e390a6ac7ea883289b24f95b
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-09-01 21:57:32 +0530

    handle dashboad route and created html file

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 const path = require('path');
 
 
+app.get('/', (req, res) => {
 
+    const filePath = path.join(__dirname + '/public/index.html');
+    return res.sendFile(filePath);
+});
 
 app.listen(8080, (err) => {
     if (err) {
